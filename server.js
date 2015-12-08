1d31a284beb5b57cc1fb72715f0de9f364f3bb68
Donald Wasserman <djwasserman@gmail.com>
2015-12-08 06:40:06 -0500

    ditched stupid cors module

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ Promise.promisifyAll(fs);
 app.use(express.static(__dirname + '/public'));
 
 app.use(function(req, res, next) {
-    cors();
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
 
